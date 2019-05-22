3a26c379d55242608356822f0f84895c11b15627
mrc <marcoambrosini@pm.me>
2019-05-22 17:16:32 +0200

    functional programming

---
+++ functional-programming/05.js
@@ -0,0 +1,17 @@
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+  // Add your code below this line
+
+  for (let i of this) {
+    newArray.push(callback(i));
+  }
+  // Add your code above this line
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
---
+++ functional-programming/07.js
@@ -0,0 +1,18 @@
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback) {
+  var newArray = [];
+  // Add your code below this line
+  for (let i of this) {
+    if (callback(i)) {
+      newArray.push(i);
+    }
+  }
+  // Add your code above this line
+  return newArray;
+};
+
+var new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
+});
